9a05ee3ab5287f087acc7c25a95d50dd8ead3253
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-07-16 18:12:28 +0900

    Update 1667-fix-names-in-a-table.sql

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-# UPDATE Users SET name = CONCAT(UCASE(LEFT(name, 1)),SUBSTRING(name, 2));
+# CONCAT 여러 문자열을 하나의 문자열로 합치기
+# SUBSTRING(문자열, 시작위치,길이) 문자열 자리기 함수
 SELECT user_id,
 CONCAT(UPPER(SUBSTRING(name, 1,1)),LOWER(SUBSTRING(name, 2))) as name FROM Users
 ORDER BY user_id;
