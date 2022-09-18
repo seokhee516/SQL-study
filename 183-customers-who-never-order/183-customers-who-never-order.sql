e547ed7bbba17d3a0e3260dc71f34cf057e28a10
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-09-18 18:30:21 +0900

    Time: 1194 ms (5.73%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-SELECT name as Customers From Customers
-WHERE id NOT IN (SELECT customerId FROM Orders);
+SELECT c.name AS Customers
+FROM Customers AS c
+    LEFT JOIN ORDERS AS o ON c.id = o.customerId
+WHERE o.id IS Null 
+# WHERE o.id = Null 하면 못찾음 NULL 은 특수한 값이기 때문 
